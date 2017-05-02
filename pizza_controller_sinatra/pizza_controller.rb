7d6206a57b7a9863d756bbcef8f4fa78597f9a32
Ally Gerrie <alasdairwgerrie@gmail.com>
2017-05-02 22:30:45 +0100

    adding all files

---
+++ pizza_controller_sinatra/pizza_controller.rb
@@ -0,0 +1,62 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative( 'models/pizza.rb' )
+
+# INDEX - READ - see all pizza orders
+
+get '/pizzas' do  #always plural
+  @pizzas = Pizza.all()
+  erb(:index)
+end
+
+
+#  NEW - CREATE - New pizza order - Form
+
+get '/pizzas/new' do
+  erb(:new)
+end
+
+
+# SHOW - READ - Find one pizza by id
+
+get '/pizzas/:id' do
+  @pizza = Pizza.find(params[:id])
+  erb(:show)
+end
+
+
+#CREATE - CREATE POST request
+
+post '/pizzas' do
+  @pizza = Pizza.new(params)
+  @pizza.save()
+  erb(:create)
+end
+
+
+#EDIT - change order details
+get '/pizzas/:id/edit' do
+  @pizza = Pizza.find(params[:id])
+  erb(:edit)
+end
+
+
+#UPDATE - send changes to db
+
+post '/pizzas/:id' do
+  @pizza = Pizza.new(params)
+  @pizza.update()
+  erb(:update)
+end
+
+
+# DELETE - Delete order by id
+
+post '/pizzas/:id/delete' do
+  @pizza = Pizza.find(params[:id])
+  @pizza.delete()
+  erb(:delete)
+end
+
+
